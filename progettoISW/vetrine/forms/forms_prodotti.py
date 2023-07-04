49abb56c2695dcdda9c7b7a500b2d5787dd4936a
Matteo <95751874+Troddio@users.noreply.github.com>
2023-07-05 00:13:26 +0200

    File CSS
    Aggiunte file css per le pagine vetrine (admin-utente), inserisci prodotto e carrello.

--- progettoISW/vetrine/forms/forms_prodotti.py
+++ progettoISW/vetrine/forms/forms_prodotti.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from utente.models import Prodotto
 
@@ -8,6 +9,22 @@ class NuovoProdottoForm(forms.ModelForm):
     class Meta:
         model = Prodotto
         fields = ["nome", "codice_seriale", "tipologia", "descrizione", "prezzo", "disponibilita"]
+        labels = {
+            "nome": _("Nome"),
+            "codice_seriale": _("Codice seriale"),
+            "tipologia": _("Tipologia"),
+            "descrizione": _("Descrizione"),
+            "prezzo": _("Prezzo"),
+            "disponibilita": _("Disponibilità"),
+        }
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'codice_seriale': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipologia': forms.TextInput(attrs={'class': 'form-control'}),
+            'descrizione': forms.TextInput(attrs={'class': 'form-control'}),
+            'prezzo': forms.TextInput(attrs={'class': 'form-control'}),
+            'disponibilita': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class ModificaProdottoForm(forms.ModelForm):
